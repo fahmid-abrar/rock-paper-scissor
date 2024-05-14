2698381437321819a9ef60e102c07bda835aa5c9
fahmid-abrar <167901076+fahmid-abrar@users.noreply.github.com>
2024-05-14 12:24:24 +0600

    Scoreboard added.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+let score = {
+  win: 0,
+  loss: 0,
+  tie: 0,
+  showScore: function(){
+    return `Won ${this.win}, Lost ${this.loss}, Tied ${this.tie}.`;
+  }
+};
+
 function cpuChoice() {
   let num = Math.random() * 3;
   if (num > 0 && num < 1) {
@@ -11,33 +20,53 @@ function cpuChoice() {
   }
 }
 
-console.log(`number is ${num}, Cpu chose ${cpuChoice()}`);
-
 function result(myChoice, cpuChoice) {
   if (myChoice == "rock") {
     if (cpuChoice == "rock") {
+      score.tie++;
       return "Game tied.";
     } else if (cpuChoice == "paper") {
+      score.loss++;
       return "Cpu won.";
     } else if (cpuChoice == "scissor") {
+      score.win++;
       return "You won.";
     }
   } else if (myChoice == "paper") {
     if (cpuChoice == "paper") {
+      score.tie++;
       return "Game tied.";
     } else if (cpuChoice == "scissor") {
+      score.loss++;
       return "Cpu won.";
     } else if (cpuChoice == "rock") {
+      score.win++;
       return "You won.";
     }
   }
   if (myChoice == "scissor") {
     if (cpuChoice == "scissor") {
+      score.tie++;
       return "Game tied.";
     } else if (cpuChoice == "rock") {
+      score.loss++;
       return "Cpu won.";
     } else if (cpuChoice == "paper") {
+      score.win++;
       return "You won.";
     }
   }
 }
+
+function showResult(myChoice, pcChoice, pickWinner){
+  return alert(` 
+  You have chosen ${myChoice}.
+  CPU has chosen ${pcChoice}.
+  
+  ${pickWinner}
+  ${score.showScore()}
+  
+  `);
+} 
+
+
